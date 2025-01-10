7c3827f4c510bfbbf20b06349f7cf2ffa30d498b
Zetaphor <zetaphor@hey.com>
2025-01-10 11:55:37 -0600

    Add linux launch script for Steam-free client

---
+++ launch-anyland-linux.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export STEAM_COMPAT_CLIENT_INSTALL_PATH=~/.local/share/Steam
+export STEAM_COMPAT_DATA_PATH=~/.local/share/Steam/steamapps/compatdata/505700
+
+~/.steam/root/compatibilitytools.d/GE-Proton9-20-rtsp16/proton run ~/Downloads/Anyland-Steam-Install_14-November-2022/anyland.exe
