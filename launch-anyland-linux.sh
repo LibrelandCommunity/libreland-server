4f2f369856656b87d71120a8f2f0093d85ada950
Zetaphor <zetaphor@hey.com>
2025-01-11 16:27:08 -0600

    Add bepinex to launch script

--- launch-anyland-linux.sh
+++ launch-anyland-linux.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-
 export STEAM_COMPAT_CLIENT_INSTALL_PATH=~/.local/share/Steam
 export STEAM_COMPAT_DATA_PATH=~/.local/share/Steam/steamapps/compatdata/505700
+export PRESSURE_VESSEL_FILESYSTEMS_RW=$XDG_RUNTIME_DIR/wivrn_comp_ipc
+export WINEDLLOVERRIDES="winhttp=n,b"
+export DOORSTOP_ENABLE=TRUE
+export DOORSTOP_INVOKE_DLL_PATH="BepInEx/core/BepInEx.Preloader.dll"
 
 ~/.steam/root/compatibilitytools.d/GE-Proton9-20-rtsp16/proton run ~/Downloads/Anyland-Steam-Install_14-November-2022/anyland.exe
